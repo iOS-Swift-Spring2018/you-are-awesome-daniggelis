03ae931ae83c9b9f1b6b4f5ecfa8b17ab9fa0b14
Tiffany Daniggelis <daniggel@bc.edu>
2018-01-21 14:19:46 -0500

    Initial Commit

---
+++ You are Awesome!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You are Awesome!
+//
+//  Created by Tiffany Daniggelis on 1/21/18.
+//  Copyright © 2018 Daniggelis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
