62ed30e6810f8d4b4cac27199855b4dbca0669c5
Tiffany Daniggelis <daniggel@bc.edu>
2018-01-22 17:19:23 -0500

    first file upload

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    var index = 0
+    
+    
+    // Code below executes when app first loads.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        let messages = ["You are fantastic!",
+                        "You are great!",
+                        "When the genius bar need help, they call you!",
+                        "You brighten my day!"]
+   
+        messageLabel.text = messages[index]
+        index+=1
+        
+        if index == messages.count{
+            index = 0
+        }
+        
+//        let message1 = "You are da Bomb!"
+//        let message2 = "You are great!"
+//        let message3 = "You are amazing!"
+//        if messageLabel.text == message1{
+//            messageLabel.text = message2
+//        }
+//        else if messageLabel.text == message2{
+//            messageLabel.text = message3
+//        }
+//        else{
+//            messageLabel.text = message1
+//        }
+        
     }
-
-
+    
 }
 
